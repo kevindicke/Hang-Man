4bd5a9446d2e7f53eef1d334c6e1f84546e16c24
K.Dicke <k.dicke7@gmail.com>
2017-04-03 16:36:20 -0700

    add js for letter comparison

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 var p2word = ""
 
-$('.p1btn').on('click',function(evt){
-  evt.preventDefault()
-  $('.p1form').css('display', 'none')
-  p2word = $('.p1input').val()
-  dashDisplay()
+$('.p1btn').on('click', function(evt) {
+    evt.preventDefault()
+    $('.p1form').css('display', 'none')
+    p2word = $('.p1input').val()
+    dashDisplay()
 })
 
-dashDisplay = function(){
-  for(var i = 0; i < p2word.length; i += 1){
-    $('.dashes').append('<span> _ </span>')
+function dashDisplay() {
+    for (var i = 0; i < p2word.length; i += 1) {
+        $('.dashes').append('<span> _ </span>')
+    }
+}
+$('.letterInput').on('keyup change', letterCheck)
+
+function letterCheck() {
+  if (p2word.includes($('.letterInput').val())) {
+    console.log('found match')
+    for (var i = 0; i < p2word.length; i += 1) {
+        if(p2word[i] == $('.letterInput').val()) {
+          // $('.dashes').find('span').eq(i).text($('.letterInput').val())
+          $('.dashes span').eq(i).text($('.letterInput').val())
+        }
+    }
+  } else {
+    $('.missed').append('<span>' + $('.letterInput').val() + '</span>')
   }
+  $('.letterInput').val('')
 }
